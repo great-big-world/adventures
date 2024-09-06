2cbeeab77bc449f7ffd7a1df6eb11c78ea164fd6
Jack Zisa <33011337+jack-zisa@users.noreply.github.com>
2024-09-06 19:44:28 -0400

    Update GameRendererMixin.java

--- src/main/java/dev/creoii/greatbigworld/adventures/mixin/client/GameRendererMixin.java
+++ src/main/java/dev/creoii/greatbigworld/adventures/mixin/client/GameRendererMixin.java
@@ -15,7 +15,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class GameRendererMixin {
     @Inject(method = "getNightVisionStrength", at = @At("RETURN"), cancellable = true)
     private static void gbw$amplifierAffectsNightVision(LivingEntity entity, float tickDelta, CallbackInfoReturnable<Float> cir, @Local StatusEffectInstance statusEffectInstance) {
-        float a = Math.min(.1f * (entity.getStatusEffect(StatusEffects.NIGHT_VISION).getAmplifier() + 1), 5f);
+        float a = Math.min(.2f * (entity.getStatusEffect(StatusEffects.NIGHT_VISION).getAmplifier() + 1), 5f);
         cir.setReturnValue(!statusEffectInstance.isDurationBelow(200) ? a : (a * .7f) + MathHelper.sin((float) ((statusEffectInstance.getDuration() - tickDelta) * Math.PI * .2f)) * .3f);
     }
 }
