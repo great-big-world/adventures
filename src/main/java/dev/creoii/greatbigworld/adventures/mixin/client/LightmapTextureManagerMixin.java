c0e1cee25ef7622f432a3aaa140abb2d7e0f457c
Jack Zisa <33011337+jack-zisa@users.noreply.github.com>
2024-09-07 14:47:09 -0400

    Fix undergroundness ignoring leaves

--- src/main/java/dev/creoii/greatbigworld/adventures/mixin/client/LightmapTextureManagerMixin.java
+++ src/main/java/dev/creoii/greatbigworld/adventures/mixin/client/LightmapTextureManagerMixin.java
@@ -25,7 +25,7 @@ public class LightmapTextureManagerMixin {
         if (entity instanceof ClientPlayerEntity clientPlayer && clientPlayer.isSpectator())
             return original.call(instance, entity, factor, delta);
 
-        float undergroundness = UndergroundHelper.sampleLightAt(client.world, entity.getBlockPos(), LightType.SKY);
+        float undergroundness = UndergroundHelper.sampleLightAtIgnoreLeaves(client.world, entity.getBlockPos(), LightType.SKY);
         float darkness = original.call(instance, entity, factor, delta) - ((1f - undergroundness) * .5f);
         if (client.world != null) {
             return MathHelper.lerp(undergroundness, darkness, darkness - MOON_PHASE_BRIGHTNESS[client.world.getMoonPhase()] * getTimeInfluence());
--- src/main/java/dev/creoii/greatbigworld/adventures/util/UndergroundHelper.java
+++ src/main/java/dev/creoii/greatbigworld/adventures/util/UndergroundHelper.java
@@ -1,6 +1,8 @@
 package dev.creoii.greatbigworld.adventures.util;
 
+import net.minecraft.registry.tag.BlockTags;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.Heightmap;
 import net.minecraft.world.LightType;
 import net.minecraft.world.World;
 
@@ -15,6 +17,23 @@ public final class UndergroundHelper {
         for (BlockPos pos : BlockPos.iterate(center.add(-1, -1, -1), center.add(1, 2, 1))) {
             if (!world.getBlockState(pos).isOpaque()) {
                 light += world.getLightLevel(lightType, pos);
+                ++total;
+            }
+        }
+
+        return ((float) light / total) / 15f;
+    }
+
+    public static float sampleLightAtIgnoreLeaves(World world, BlockPos center, LightType lightType) {
+        int total = 0;
+        int light = 0;
+
+        for (BlockPos pos : BlockPos.iterate(center.add(-1, -1, -1), center.add(1, 2, 1))) {
+            if (!world.getBlockState(pos).isOpaque()) {
+                BlockPos y = world.getTopPosition(Heightmap.Type.MOTION_BLOCKING, pos);
+                if (y.getY() >= pos.getY() && world.getBlockState(y.down()).isIn(BlockTags.LEAVES)) {
+                    light += world.getLightLevel(lightType, y);
+                } else light += world.getLightLevel(lightType, pos);
                 ++total;
             }
         }
