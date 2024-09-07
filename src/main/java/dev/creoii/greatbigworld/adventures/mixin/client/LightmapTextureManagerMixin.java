53de2b64dff7d7eee54f2f4791eb67947c485541
Jack Zisa <33011337+jack-zisa@users.noreply.github.com>
2024-09-06 20:43:55 -0400

    Don't apply darkness modifiers to spectators

--- src/main/java/dev/creoii/greatbigworld/adventures/mixin/client/LightmapTextureManagerMixin.java
+++ src/main/java/dev/creoii/greatbigworld/adventures/mixin/client/LightmapTextureManagerMixin.java
@@ -22,12 +22,14 @@ public class LightmapTextureManagerMixin {
 
     @WrapOperation(method = "update", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/LightmapTextureManager;getDarkness(Lnet/minecraft/entity/LivingEntity;FF)F"))
     private float gbw$modifyDarknessForMoonPhase(LightmapTextureManager instance, LivingEntity entity, float factor, float delta, Operation<Float> original) {
+        if (entity instanceof ClientPlayerEntity clientPlayer && clientPlayer.isSpectator())
+            return original.call(instance, entity, factor, delta);
+
         float undergroundness = UndergroundHelper.sampleLightAt(client.world, entity.getBlockPos(), LightType.SKY);
         float darkness = original.call(instance, entity, factor, delta) - ((1f - undergroundness) * .5f);
-        if (client.world != null && entity instanceof ClientPlayerEntity) {
+        if (client.world != null) {
             return MathHelper.lerp(undergroundness, darkness, darkness - MOON_PHASE_BRIGHTNESS[client.world.getMoonPhase()] * getTimeInfluence());
-        }
-        return darkness;
+        } return darkness;
     }
 
     @Unique
