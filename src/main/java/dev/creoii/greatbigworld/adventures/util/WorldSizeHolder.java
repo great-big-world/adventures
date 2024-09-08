30d85d00abd0a0363615d792e2ffd14fb36b3e07
Jack Zisa <33011337+jack-zisa@users.noreply.github.com>
2024-09-08 15:12:16 -0400

    Fix weird water flowing behavior

--- src/main/java/dev/creoii/greatbigworld/adventures/util/WorldSizeHolder.java
+++ src/main/java/dev/creoii/greatbigworld/adventures/util/WorldSizeHolder.java
@@ -1,11 +1,18 @@
 package dev.creoii.greatbigworld.adventures.util;
 
+import net.minecraft.util.math.BlockPos;
+
 public interface WorldSizeHolder {
     void gbw$setWorldSize(int worldSize);
 
     int gbw$getWorldSize();
 
-    static boolean isWithinWorld(WorldSizeHolder chunkGenerator, int chunkX, int chunkZ) {
-        return chunkGenerator.gbw$getWorldSize() > 0 && (chunkX >= chunkGenerator.gbw$getWorldSize() || chunkX < -chunkGenerator.gbw$getWorldSize() || chunkZ >= chunkGenerator.gbw$getWorldSize() || chunkZ < -chunkGenerator.gbw$getWorldSize());
+    static boolean isWithinWorld(WorldSizeHolder worldSizeHolder, int chunkX, int chunkZ) {
+        return worldSizeHolder.gbw$getWorldSize() > 0 && (chunkX >= worldSizeHolder.gbw$getWorldSize() || chunkX < -worldSizeHolder.gbw$getWorldSize() || chunkZ >= worldSizeHolder.gbw$getWorldSize() || chunkZ < -worldSizeHolder.gbw$getWorldSize());
+    }
+
+    static boolean isWithinWorld(WorldSizeHolder worldSizeHolder, BlockPos pos) {
+        int sizeInBlocks = worldSizeHolder.gbw$getWorldSize() * 16;
+        return sizeInBlocks > 0 && (pos.getX() >= sizeInBlocks || pos.getX() < -sizeInBlocks || pos.getZ() >= sizeInBlocks || pos.getZ() < -sizeInBlocks);
     }
 }
