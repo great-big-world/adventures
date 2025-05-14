aa2308d27080f65fdc32a6b2ad0f8f2a3ba423bf
Jack Zisa <33011337+jack-zisa@users.noreply.github.com>
2025-05-13 23:25:08 -0400

    Fix world size of -1 resulting in void worlds

--- src/main/java/dev/creoii/greatbigworld/adventures/util/WorldSizeHolder.java
+++ src/main/java/dev/creoii/greatbigworld/adventures/util/WorldSizeHolder.java
@@ -6,6 +6,8 @@ public interface WorldSizeHolder {
     int gbw$getWorldSize();
 
     static boolean isOutsideWorld(WorldSizeHolder worldSizeHolder, int chunkX, int chunkZ) {
+        if (worldSizeHolder.gbw$getWorldSize() == -1)
+            return false;
         if (worldSizeHolder.gbw$getWorldSize() == 0) {
             return chunkX > 0 || chunkZ > 0;
         } else return chunkX >= worldSizeHolder.gbw$getWorldSize() || chunkX < -worldSizeHolder.gbw$getWorldSize() || chunkZ >= worldSizeHolder.gbw$getWorldSize() || chunkZ < -worldSizeHolder.gbw$getWorldSize();
