d30efc297bd3f7fd81917c2c8deda7c4000511cc
Jack Zisa <33011337+jack-zisa@users.noreply.github.com>
2025-11-28 10:11:47 -0500

    Fix trees affecting darkness

--- src/main/java/dev/creoii/greatbigworld/adventures/mixin/client/LightmapTextureManagerMixin.java
+++ src/main/java/dev/creoii/greatbigworld/adventures/mixin/client/LightmapTextureManagerMixin.java
@@ -2,6 +2,7 @@ package dev.creoii.greatbigworld.adventures.mixin.client;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import dev.creoii.greatbigworld.adventures.client.AdventuresClient;
 import dev.creoii.greatbigworld.adventures.util.UndergroundHelper;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.LightmapTextureManager;
@@ -31,21 +32,25 @@ public class LightmapTextureManagerMixin {
             float abovegroundDarkness = darkness - (MOON_PHASE_BRIGHTNESS[moonPhase] * getTimeInfluence());
             float undergroundDarkness = darkness + .105f;
 
-            return MathHelper.lerp(UndergroundHelper.sampleLightAt(client.world, entity.getBlockPos(), LightType.SKY), undergroundDarkness, abovegroundDarkness);
+            return MathHelper.lerp(UndergroundHelper.sampleLight(client.world, entity.getBlockPos(), LightType.SKY), undergroundDarkness, abovegroundDarkness);
         }
         return darkness;
     }
 
     @Unique
     private float getTimeInfluence() {
-        long timeOfDay = client.world.getTimeOfDay() % 24000L;
-        if (timeOfDay >= 12000L && timeOfDay < 15000L) {
-            return (float) (timeOfDay - 12000L) / 3000f;
-        } else if (timeOfDay >= 15000L && timeOfDay < 23000L) {
+        long dayLength = AdventuresClient.dayLength;
+        long nightLength = AdventuresClient.nightLength;
+        long total = dayLength + nightLength;
+
+        long timeOfDay = client.world.getTimeOfDay() % total;
+        if (timeOfDay >= dayLength && timeOfDay < dayLength + (nightLength * .3d)) {
+            return (float) (timeOfDay - dayLength) / 3000f;
+        } else if (timeOfDay >= (nightLength * .3d) && timeOfDay < (total - 1000L)) {
             return 1f;
-        } else if (timeOfDay >= 22000L || timeOfDay < 1000L) {
-            if (timeOfDay >= 22000L) {
-                return (float) (24000L - timeOfDay) / 3000f;
+        } else if (timeOfDay >= (total - 2000L) || timeOfDay < 1000L) {
+            if (timeOfDay >= (total - 2000L)) {
+                return (float) (total - timeOfDay) / 3000f;
             } else return (float) (1000L - timeOfDay) / 3000f;
         }
         return 0f;
--- src/main/java/dev/creoii/greatbigworld/adventures/util/UndergroundHelper.java
+++ src/main/java/dev/creoii/greatbigworld/adventures/util/UndergroundHelper.java
@@ -2,79 +2,34 @@ package dev.creoii.greatbigworld.adventures.util;
 
 import net.minecraft.block.BlockState;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.Heightmap;
 import net.minecraft.world.LightType;
 import net.minecraft.world.World;
 
-import java.util.*;
-
 public final class UndergroundHelper {
-    private static final BlockPos.Mutable MUTABLE = new BlockPos.Mutable();
-    private static final Set<Integer> YS = new HashSet<>();
-
     /**
      * @return A float from 0-1 determining how much skylight is around the center position.
      */
-    public static float sampleLightAt(World world, BlockPos center, LightType lightType) {
+    public static float sampleLight(World world, BlockPos center, LightType lightType) {
         int total = 0;
-        int light = 0;
+        int lightSum = 0;
 
         for (BlockPos pos : BlockPos.iterate(center.add(-1, -1, -1), center.add(1, 2, 1))) {
-            if (!world.getBlockState(pos).isOpaque()) {
-                light += world.getLightLevel(lightType, pos);
-                ++total;
-            }
-        }
+            BlockState state = world.getBlockState(pos);
 
-        return ((float) light / total) / 15f;
-    }
+            int opacity = state.getOpacity();
 
-    public static float sampleLightAtIgnoreNonOpaque(World world, BlockPos center, LightType lightType) {
-        int total = 0;
-        int light = 0;
-
-        int add;
-        for (BlockPos pos : BlockPos.iterate(center.add(-1, -1, -1), center.add(1, 2, 1))) {
-            if (!world.getBlockState(pos).isOpaqueFullCube()) {
-                add = world.getLightLevel(lightType, pos);
-
-                if (add == 15) {
-                    light += 15;
-                    ++total;
-                    continue;
-                }
-
-                MUTABLE.set(world.getTopPosition(Heightmap.Type.MOTION_BLOCKING, pos));
-                if (!world.getBlockState(MUTABLE).isOpaqueFullCube() && isNonOpaqueBetween(world, pos)) {
-                    light += 15;
-                } else light += add;
-
-                // only count blocks that accept light
-                ++total;
-            }
-        }
-
-        // average of all light values / 15f to normalize
-        return total > 0 ? ((float) light / total) / 15f : 0f;
-    }
-
-    public static boolean isNonOpaqueBetween(World world, BlockPos bottom) {
-        if (MUTABLE.getY() - bottom.getY() <= 1)
-            return world.getBlockState(bottom).isOpaqueFullCube();
-
-        YS.clear();
-        for (int y = MUTABLE.getY(); y >= bottom.getY(); y -= y < world.getSeaLevel() ? 2 : 1) {
-            YS.add(y);
-        }
-
-        for (int y : YS) {
-            MUTABLE.setY(y);
-            BlockState state = world.getBlockState(MUTABLE);
-            if (state.isAir())
+            if (opacity >= 15)
                 continue;
-            if (state.isOpaqueFullCube())
-                return false;
+
+            int sample = world.getLightLevel(lightType, pos);
+            if (lightType == LightType.SKY) {
+                sample += Math.clamp(15 - opacity, 0, 15);
+            }
+
+            lightSum += sample;
+            total++;
         }
-        return true;
+
+        return total > 0 ? (lightSum / (float) total) / 15f : 0f;
     }
 }
