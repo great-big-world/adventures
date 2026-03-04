f57d16ebf0aa2fd090012285a87226c76691e311
Jack Zisa <33011337+jack-zisa@users.noreply.github.com>
2026-03-04 00:33:39 -0500

    Improve mixin & more code removals

--- src/main/java/dev/creoii/greatbigworld/adventures/item/AstrolabeItem.java
+++
@@ -1,9 +0,0 @@
-package dev.creoii.greatbigworld.adventures.item;
-
-import net.minecraft.world.item.Item;
-
-public class AstrolabeItem extends Item {
-    public AstrolabeItem(Properties settings) {
-        super(settings);
-    }
-}
--- src/main/java/dev/creoii/greatbigworld/adventures/mixin/client/LevelLoadingScreenMixin.java
+++ src/main/java/dev/creoii/greatbigworld/adventures/mixin/client/LevelLoadingScreenMixin.java
@@ -1,5 +1,7 @@
 package dev.creoii.greatbigworld.adventures.mixin.client;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import dev.creoii.greatbigworld.client.GreatBigWorldClient;
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
@@ -8,15 +10,14 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.resources.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(LevelLoadingScreen.class)
 public class LevelLoadingScreenMixin {
-    @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;drawCenteredString(Lnet/minecraft/client/gui/Font;Lnet/minecraft/network/chat/Component;III)V"))
-    private void gbw$addDimensionToLoadingText(GuiGraphics instance, Font textRenderer, Component text, int centerX, int y, int color) {
+    @WrapOperation(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;drawCenteredString(Lnet/minecraft/client/gui/Font;Lnet/minecraft/network/chat/Component;III)V"))
+    private void gbw$addDimensionToLoadingText(GuiGraphics instance, Font font, Component component, int i, int j, int k, Operation<Void> original) {
         Identifier dimensionId = GreatBigWorldClient.getToDimension();
         if (dimensionId != null)
-            instance.drawCenteredString(textRenderer, Component.translatable("multiplayer.downloadingTerrain.targeted", Component.translatable("dimension_type." + dimensionId.getNamespace() + "." + dimensionId.getPath())), centerX, y, color);
-        else instance.drawCenteredString(textRenderer, text, centerX, y, color);
+            instance.drawCenteredString(font, Component.translatable("multiplayer.downloadingTerrain.targeted", Component.translatable("dimension_type." + dimensionId.getNamespace() + "." + dimensionId.getPath())), i, j, k);
+        else instance.drawCenteredString(font, component, i, j, k);
     }
 }
