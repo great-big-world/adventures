76a19af8f249db9c5131358a15059c6bda399431
Jack Zisa <33011337+jack-zisa@users.noreply.github.com>
2026-08-25 19:16:33 -0400

    Remove ladder sprint in favor of submod

--- src/main/java/dev/creoii/greatbigworld/adventures/mixin/entity/LivingEntityMixin.java
+++ src/main/java/dev/creoii/greatbigworld/adventures/mixin/entity/LivingEntityMixin.java
@@ -1,18 +1,11 @@
 package dev.creoii.greatbigworld.adventures.mixin.entity;
 
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
-import net.minecraft.core.Direction;
-import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin {
@@ -22,17 +15,5 @@ public abstract class LivingEntityMixin {
     @Inject(method = "stopSleeping", at = @At("TAIL"))
     private void gbw$healOnWakeUp(CallbackInfo ci) {
         heal(getMaxHealth() / 5f);
-    }
-
-    @ModifyExpressionValue(method = "handleOnClimbable", at = @At(value = "INVOKE", target = "Ljava/lang/Math;max(DD)D"))
-    private double gbw$handleClimbSprinting(double g) {
-        LivingEntity living = (LivingEntity) (Object) this;
-
-        Direction moveDir = g < 0d ? Direction.DOWN : Direction.UP;
-        if (living.isSprinting() && moveDir == Direction.getFacingAxis(living, Direction.Axis.Y)) {
-            g += moveDir == Direction.DOWN ? -.1d : .1d;
-        }
-
-        return g;
     }
 }
